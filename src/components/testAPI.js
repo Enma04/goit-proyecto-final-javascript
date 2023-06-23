b0952e385cb869b45ce7553b60b8fda374386c25
Byron-Poveda <119381029+Byron-Poveda@users.noreply.github.com>
2023-06-23 00:04:39 -0500

    Add btns pagination

--- src/components/testAPI.js
+++ src/components/testAPI.js
@@ -1,97 +1,119 @@
 // importaciones y declaraciones
 import axios from 'axios';
+import 'remixicon/fonts/remixicon.css';
 const apiKey = '6617c9b64f7274de96d2c2a2c77c593e';
-let page = 1;
-let vec;
+let i = 1;
 const imageBaseURL = 'https://image.tmdb.org/t/p/w500';
 const listApi = document.getElementById('list_api');
-const anotherBtn = document.getElementById('anotherPage');
-const previousBtn = document.getElementById('previousPage');
+const listPages = document.getElementById('listPages');
 
 // funcion q genera los items con sus respectivos valores sacados de la api de TMDB
 let addImages = page => {
   axios
     .get(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&include_adult=false&include_video=false&language=es-ES&page=${page}&sort_by=popularity.desc`
+      `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&include_adult=true&include_video=false&language=es-ES&page=${page}&sort_by=popularity.desc&`
     )
     .then(res => {
-      vec = res.data.results;
-      //console.log(res.data.results);
       res.data.results.forEach(element => {
+        vec = res.data.results;
         listApi.insertAdjacentHTML(
           'beforeend',
           `<li>
         <h3>${element.original_title}</h3>
-        <img src="${imageBaseURL}${element.backdrop_path}" alt="${element.title}" />
+        <img src="${imageBaseURL}${element.poster_path}" alt="${element.title}" />
         <p>${element.overview}</p>
         <button class="mylistBTN">Add to My List</button>
       </li>`
         );
       });
+      let allPages = res.data.total_pages;
+      function elem(perPage, page) {
+        let li = '';
+        let beforePages = page;
+        let afterPages = page + 1;
+        let listActive;
+        if (page > 1) {
+          li += `<li class="btn" id="previousPage"><i class="ri-arrow-left-s-line"></i></li>`;
+        }
+        for (let i = beforePages; i < afterPages; i += 1) {
+          if (i > allPages) {
+            continue;
+          }
+
+          if (page === i) {
+            listActive = 'active';
+          } else {
+            listActive = '';
+          }
+          li += `<li class="numb ${listActive}">${i}</li>`;
+        }
+        if (page < perPage) {
+          li += `<li class="btn" id="anotherPage"><i class="ri-arrow-right-s-line"></i></li>`;
+        }
+        listPages.innerHTML = li;
+
+        // Agregar eventos onclick a los elementos de paginación
+        const previousBtn = document.getElementById('previousPage');
+        const anotherBtn = document.getElementById('anotherPage');
+
+        if (previousBtn) {
+          previousBtn.addEventListener('click', () => {
+            i--; // Restar 1 a i
+            elem(allPages, i);
+            addImages(i); // Volver a llamar a elem con la nueva página
+            listApi.innerHTML = '';
+          });
+        }
+
+        if (anotherBtn) {
+          anotherBtn.addEventListener('click', () => {
+            i++; // Sumar 1 a i
+            elem(allPages, i);
+            addImages(i);
+            listApi.innerHTML = '';
+          });
+        }
+      }
+      // ejecuto la función para mostrar la paginación inicial
+      elem(allPages, i);
     })
     .catch(err => console.error('error: ' + err));
 };
 // ejecuto la funcion
-addImages(page);
-
-// sumo a la paginacion +1 y ejecuto la funcion
-anotherBtn.addEventListener('click', () => {
-  listApi.textContent = '';
-  page++;
-  btnPage();
-  addImages(page);
-});
-
-// resto a la paginacion -1 y ejecuto la funcion
-previousBtn.addEventListener('click', () => {
-  listApi.textContent = '';
-  page--;
-  btnPage();
-  addImages(page);
-});
-const btnPage = () => {
-  if (page === 1) {
-    previousBtn.style.display = 'none';
-  } else {
-    previousBtn.style.display = 'unset';
-  }
-};
-btnPage();
-
+addImages(i);
 
 //------------------------------------------------------------------------
 // --------------- SECTION FOR MY LIST
 let count = 0;
 
-if( localStorage.length !== 0 ) {
-  count = localStorage.getItem("conteo");
+if (localStorage.length !== 0) {
+  count = localStorage.getItem('conteo');
 }
 
-listApi.addEventListener("click", event => {
-  if (event.target.nodeName !== "BUTTON") {
+listApi.addEventListener('click', event => {
+  if (event.target.nodeName !== 'BUTTON') {
     return;
   }
 
-  let name = event.target.parentElement.childNodes[0].nextElementSibling.innerHTML;
+  let name =
+    event.target.parentElement.childNodes[0].nextElementSibling.innerHTML;
 
-  for(let i=0; i<vec.length; i++) {
-    if( vec[i].original_title === name ) {
+  for (let i = 0; i < vec.length; i++) {
+    if (vec[i].original_title === name) {
       //if(localStorage.length <= 0) {
-        for(let j=1; j<=localStorage.getItem("conteo"); j++) {
-          if( JSON.parse(localStorage.getItem(`${j}`)).original_title === name ) {
-            return
-          }
+      for (let j = 1; j <= localStorage.getItem('conteo'); j++) {
+        if (JSON.parse(localStorage.getItem(`${j}`)).original_title === name) {
+          return;
         }
+      }
       //}
       count++;
       localStorage.setItem(`${count}`, JSON.stringify(vec[i]));
-      localStorage.setItem("conteo", `${count}`);
+      localStorage.setItem('conteo', `${count}`);
       //localStorage.clear();
       //count = 0;
     }
   }
 
-  console.log("Hice click!", name);
+  console.log('Hice click!', name);
 });
-
-

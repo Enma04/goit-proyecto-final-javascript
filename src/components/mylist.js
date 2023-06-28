9d125a0a246a7c6583178b4749bcf602a7393ae9
Enma04 <programmer1ebg@gmail.com>
2023-06-28 14:49:51 -0500

    Mylibrary.js con funciones

--- src/components/mylist.js
+++ src/components/mylist.js
@@ -1,41 +1,11 @@
-import { imageBaseURL } from './testAPI';
+import { lista, deleteItem } from './showList';
 const listaFav = document.getElementById('listaFav');
-console.log("Lista fav: ", listaFav);
-for (let i = 1; i <= JSON.parse(localStorage.getItem('watched')); i++) {
-  
-  if (localStorage.getItem(`watched${i}`) !== null) {
-    let element = JSON.parse(localStorage.getItem(`watched${i}`));
-    console.log('element = ', element);
-    
-    listaFav.insertAdjacentHTML(
-    'beforeend',
-      `<li class="card">
-        <h3>${element.original_title}</h3>
-        <img src="${imageBaseURL}${element.poster_path}" alt="${element.title}" />
-        <p>${element.overview}</p>
-        <button class="mylistBTN addedBTN">Added</button>
-      </li>`
-    );
-  }
-}
+
+lista(listaFav, 'watched');
 
 listaFav.addEventListener('click', event => {
-  if (event.target.nodeName !== 'BUTTON') {
-    return;
-  }
-
-  console.log("Hice click!");
-  let name = event.target.parentElement.childNodes[0].nextElementSibling.innerHTML;
-
-  console.log("name = ", name);
-
-  for (let j = 1; j <= JSON.parse(localStorage.getItem(`watched`)); j++) {
-    if (localStorage.getItem(`watched${j}`) !== null) {
-      if (JSON.parse(localStorage.getItem(`watched${j}`)).original_title === name) {
-        localStorage.removeItem(`watched${j}`);
-        window.location.reload();
-        return;
-      }
-    }
+  if (event.target.id === 'deleteWatched') {
+    console.log("event= ", event);
+    deleteItem(event, 'watched');
   }
 });
